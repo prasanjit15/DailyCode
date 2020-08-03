6c7701fd6b1c63df603453043416cf88c101761b
Prasannjit Mohapatra <prasanjit.mohapatra15@gmail.com>
2020-08-03 20:28:44 +0530

    Add files via upload

---
+++ PersonalPractice/MissingRepeating.cpp
@@ -0,0 +1,22 @@
+//Missing and Repeating Number
+//1st approach - sorting and then find
+//2nd approach - hashing and find
+ 
+#include<iostream>
+
+using namespace std;
+
+int partition()
+
+int find(int arr[], int n){
+	
+}
+
+int main(){
+	int a[] = {2, 5 , 6 , 3, 9, 7, 8};
+	int n = sizeof(a)/sizeof(a[]);
+	find(a, n);
+	
+	
+	return 0;
+}
