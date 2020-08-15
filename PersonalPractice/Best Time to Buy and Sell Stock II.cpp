9e2e9fe49ac2d9d161d79c9bf258ac91ee9b9d3f
Prasannjit Mohapatra <prasanjit.mohapatra15@gmail.com>
2020-08-15 11:19:48 +0530

    Add files via upload

---
+++ PersonalPractice/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,14 @@
+//Best Time to Buy and Sell Stock II
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        
+        for(int i = 1; i<prices.size(); i++){
+            if(prices[i]>prices[i-1])
+                profit += prices[i] - prices[i-1];
+        }
+        return profit;
+    }
+};
