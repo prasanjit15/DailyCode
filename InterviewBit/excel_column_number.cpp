57f0a1bf21c95c2ccb0fee14405efd5f7dabbaa2
Prasannjit Mohapatra <prasanjit.mohapatra15@gmail.com>
2021-02-09 10:40:27 +0530

    update

--- InterviewBit/excel_column_number.cpp
+++ InterviewBit/excel_column_number.cpp
@@ -1,8 +1,8 @@
 int Solution::titleToNumber(string A) {
     int ans = 0;
-    for(int i = 0; i < A.size(); i++){
-        ans *= 26;
-        ans += A[i] - 'A' + 1;
+    int n = A.size();
+    for(int i = n - 1; i >=0 ; i--){
+        ans += (A[i] - 'A' + 1) * pow(26, n - i - 1);
     }
     
     return ans;
