d4d67ce059ca1a7a2c44f64da942eda53dacbea0
Prasannjit Mohapatra <prasanjit.mohapatra15@gmail.com>
2021-03-04 09:54:19 +0530

    update

--- Codeforces/watermelon.cpp
+++ Codeforces/watermelon.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main(){
     int x;
     cin>>x;
-    if(x%2 == 0){
+    if(x%2 == 0 && x != 2){
         cout<<"YES"<<endl;
     }else{
         cout<<"NO"<<endl;
